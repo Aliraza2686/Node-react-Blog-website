d3ee8eccb236522deb1f356fdbb8d4e4dfd78fba
Aliraza2686 <aliraza034751539@gmail.com>
2021-09-12 12:43:34 -0700

    node react

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
+  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
+  mongoURI: 'mongodb://127.0.0.1:27017/blogPosts',
+  cookieKey: '123123123',
+};
